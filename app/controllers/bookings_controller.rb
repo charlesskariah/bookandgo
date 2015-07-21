24e64637ff964948fbf870871c65f77cfb678113
[Charles Skariah] <charles@RMED150LT.(none)>
2015-07-21 19:24:21 +0530

    refactor changes for booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,6 @@ class BookingsController < ApplicationController
 before_action :authenticate_user!
 
 	def index
-
 	end
 
 	def new
@@ -13,23 +12,26 @@ before_action :authenticate_user!
 	end
 
 	def create
-		@book_room = Booking.create(params.require(:booking).permit(:meeting_room_id,:starts_at,:ends_at, :agenda, :invitees)
-			.merge( user_id: current_user.id,status: false))
-		invitees_list = params[:booking][:invitees].reject {|c| c.empty?}
-		@book_room.invitees = invitees_list
+		@book_room = Booking.new(booking_params)
 		if @book_room.save
 			flash[:success] = "Meeting room is successfully booked for you!!"
 			redirect_to new_booking_path
 		else
-			if @book_room.errors.any?
-				flash[:error] = @book_room.errors.full_messages.to_sentence			
-		 	end
-		 	redirect_to new_booking_path
+			@meeting_rooms_lists = MeetingRoom.all
+		  @employees_list = User.all
+		  render new_booking_path
 		end
 	end
 
 	def booked_list
 		@booked_room_list = Booking.where(status: false)
+	end
+
+	private
+
+	def booking_params
+		params.require(:booking).permit(:meeting_room_id,:starts_at,:ends_at, :agenda, :invitees => [])
+			.merge( user_id: current_user.id,status: false)
 	end
 
 end
