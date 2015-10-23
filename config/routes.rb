469138c5af279bdf7da5502d9038837f77e9a94f
[Charles Skariah] <charles@RMED150LT.(none)>
2015-10-23 19:31:27 +0530

    changes made for views and new designs

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'bookings#index'
+  root 'bookings#booked_list'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
       end
     end
 
-    
+
   # Example resource route with sub-resources:
   #   resources :products do
   #     resources :comments, :sales
